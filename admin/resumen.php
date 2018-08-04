51022aba23fd7c4b106cc4d9e9615bb18b3790ce
Oscar Sandoval <oscarsan.penilla@gmail.com>
2018-08-04 16:05:25 -0700

    Se modifica gracias a los cambios realizados en la clase ResumTimesheet

--- admin/resumen.php
+++ admin/resumen.php
@@ -3,38 +3,18 @@
   include("../employee.php");
   include("../validar_inicio_sesion.php");
 
-  $conexion_db = new ConexionDB();
   $fecha_inicio = $_POST['fecha_inicio'];
   $fecha_fin = $_POST['fecha_fin'];
-  $paid_by = $_POST['paid_by'];
-  $site = $_POST['site'];
-  $ocupation = $_POST['ocupation'];
-  $sql = "SELECT site,ocupation,name, SUM(hours_day) AS hours, work_for_rate, SUM(hours_day)*work_for_rate AS total
-          FROM events ";
-  $sql .= "WHERE date BETWEEN '$fecha_inicio' AND '$fecha_fin'";
-  if ($paid_by != "any") { $sql .= " AND paid_by='$paid_by'";  }
-  if ($site == "VH_and_P") {
-    $sql .= " AND (site='Vancouver House' OR site='Pendrell')";
-  }else {
-    if ($site != "any") { $sql .= " AND site='$site'"; }
-  }
 
+  $resume = new ResumeTimesheet($_POST);
+  $array_resume = $resume->resume();
+  //$ocupations = $resume->getOcupations();
+  $results = $resume->getEvents();
+  $bank_results = $resume->bankResume();
+  $cash_results = $resume->cashResume();
+  $hrs = 0.0;
+  $paid = 0.0;
 
-  if ($ocupation != "any") { $sql .= " AND ocupation='$ocupation'"; }
-  $sql .= " GROUP BY name ORDER BY site,ocupation,name";
-  $results = $conexion_db->ConsultaArray($sql);
-  $sql = "SELECT name,bank_info,SUM(hours_day)*work_for_rate AS total FROM events ";
-  $sql .= "WHERE date BETWEEN '$fecha_inicio' AND '$fecha_fin'";
-  if ($paid_by != "any") { $sql .= " AND paid_by='$paid_by'";  }
-  if ($site != "any") { $sql .= " AND site='$site'"; }
-  if ($ocupation != "any") { $sql .= " AND ocupation='$ocupation'"; }
-  $sql .= " GROUP BY name ORDER BY name";
-  $results_bank = $conexion_db->ConsultaArray($sql);
-
-  $total1 = 0.0;
-  $total2 = 0.0;
-
-  echo $sql;
  ?>
 
  <!DOCTYPE html>
@@ -45,8 +25,45 @@
      <title></title>
    </head>
    <body>
-     <div class="contenedor">
+      <div class="contenedor"> <!--table main resume -->
+       <h3>Final Resume</h3>
+       <p><strong><?php echo $fecha_inicio; ?></strong> to <strong><?php echo $fecha_fin; ?></strong></p>
        <table>
+         <thead>
+           <tr>
+             <th>Site</th>
+             <th>Ocupation</th>
+             <th>Hours</th>
+             <th>Work for Paid</th>
+           </tr>
+         </thead>
+         <tbody> <!-- body main resume  -->
+           <?php $i = 0 ; foreach($array_resume as $row): ?>
+            <tr>
+              <td><?php echo $row->site; ?></td>
+              <td><?php echo $row->ocupation; ?></td>
+              <td class="td_center"><?php echo $row->hours; ?> </td>
+              <td class="td_money"><?php echo $row->total_ocupation;?></td>
+            </tr>
+            <?php
+              $hrs += $row->hours;
+              $paid += $row->total_ocupation;
+             ?>
+           <?php endforeach; ?>
+         </tbody>
+         <tfoot> <!-- footer main resume  -->
+           <tr>
+             <td colspan="2">Totals</td>
+             <td class="td_center"><strong><?php echo $hrs; ?></strong></td>
+             <td class="td_money"><strong><?php echo $paid; ?></strong></td>
+           </tr>
+         </tfoot>
+       </table>
+     </div>
+     <div class="contenedor">
+       <h3>Total Employees</h3>
+       <p><strong><?php echo $fecha_inicio; ?></strong> to <strong><?php echo $fecha_fin; ?></strong></p>
+       <table><!-- table Total employees  -->
          <thead>
            <tr>
              <th>Site</th>
@@ -57,7 +74,11 @@
              <th>Total</th>
            </tr>
          </thead>
-         <tbody>
+         <tbody> <!-- body Total employees  -->
+           <?php
+            $hrs = 0.0;
+            $paid = 0.0;
+            ?>
            <?php foreach ($results as $row):?>
              <tr>
                <td><?php echo $row->site ?> </td>
@@ -67,17 +88,28 @@
                <td class="td_center"><?php echo $row->hours ?> </td>
                <td class="td_money"><?php echo $row->total ?> </td>
              </tr>
-             <?php $total1 += $row->total ?>
+             <?php
+             $hrs += $row->hours;
+             $paid += $row->total;
+             ?>
            <?php endforeach; ?>
-            <tr>
-              <td class="td_total" colspan="6">Total: <strong>$ <?php echo $total1; ?></strong></td>
-            </tr>
          </tbody>
+         <tfoot> <!-- footer Total employees  -->
+           <tr>
+             <td>Totals</td>
+             <td></td>
+             <td class="td_center"><?php echo sizeof($results) ?></td><td></td>
+             <td class="td_center"><strong><?php echo $hrs; ?></strong></td>
+             <td class="td_money"><strong><?php echo $paid; ?></strong></td>
+           </tr>
+         </tfoot>
        </table>
      </div>
      <div class="second_table">
        <div class="contenedor">
-         <table>
+         <h3>TD Bank Deposits</h3>
+         <p><strong><?php echo $fecha_inicio; ?></strong> to <strong><?php echo $fecha_fin; ?></strong></p>
+         <table><!-- table TD Bank Deposits  -->
            <thead>
              <tr>
                <th>Name</th>
@@ -85,22 +117,57 @@
                <th>Amount</th>
              </tr>
            </thead>
-           <tbody>
-             <?php foreach ($results_bank as $row):?>
+           <tbody><!-- body TD Bank Deposits  -->
+             <?php $paid = 0.0 ?>
+             <?php foreach ($bank_results as $row):?>
                <tr>
                  <td><?php echo $row->name ?> </td>
                  <td class="td_center"><?php echo $row->bank_info ?> </td>
                  <td class="td_money"><?php echo $row->total ?> </td>
-                 <?php $total2 += $row->total ?>
+                 <?php $paid += $row->total ?>
                </tr>
              <?php endforeach; ?>
-             <tr>
-               <td class="td_total" colspan="3">Total: <strong>$ <?php echo $total2; ?></strong></td>
-             </tr>
            </tbody>
-         </table>
-       </div>
-     </div>
+           <tfoot><!-- footer TD Bank Deposits  -->
+             <tr>
+               <td>Total</td>
+               <td></td>
+               <td class="td_money" ><strong><?php echo $paid; ?></strong></td>
+             </tr>
+           </tfoot>
+         </table><!-- table end TD Bank Deposits  -->
+       </div><!-- TD Bank Deposits  -->
+     </div><!-- second table  -->
+     <div class="second_table">
+       <div class="contenedor">
+         <h3>No Bank Information</h3>
+         <p><strong><?php echo $fecha_inicio; ?></strong> to <strong><?php echo $fecha_fin; ?></strong></p>
+         <table><!-- table no bank info  -->
+           <thead>
+             <tr>
+               <th>Name</th>
+               <th>Amount</th>
+             </tr>
+           </thead>
+           <tbody><!-- body bank info  -->
+             <?php $paid = 0.0 ?>
+             <?php foreach ($cash_results as $row):?>
+               <tr>
+                 <td><?php echo $row->name ?> </td>
+                 <td class="td_money"><?php echo $row->total ?> </td>
+                 <?php $paid += $row->total ?>
+               </tr>
+             <?php endforeach; ?>
+           </tbody>
+           <tfoot><!-- footer bank info  -->
+             <tr>
+               <td>Total</td>
+               <td class="td_money" ><strong><?php echo $paid; ?></strong></td>
+             </tr>
+           </tfoot>
+         </table><!-- table end bank info  -->
+       </div><!-- TD Bank bank info  -->
+     </div><!-- second table  -->
      <br>
    </body>
  </html>
